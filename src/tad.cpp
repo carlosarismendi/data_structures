4ed615d0ba9284205a531dd6cf36db92099da296
Carlos Arismendi <ceas1@alu.ua.es>
2021-07-21 13:26:01 +0200

    Vector destructor now calls elements destructors

--- src/tad.cpp
+++ src/tad.cpp
@@ -9,22 +9,36 @@ int
 main(void)
 {
   TPoro a(1, 2, 3, (char *)"rojo");
+
+  vector<TPoro> p;
   vector<TPoro> v(5);
-
-  cout << "Cantidad: " << v.size() << endl;
-
-  v.push_back(a);
-  cout << "Cantidad: " << v.size() << endl;
+  vector<TPoro> b(v);
 
   v.push_back(a);
-  cout << "Cantidad: " << v.size() << endl;
-
   v.push_back(a);
-  cout << "Cantidad: " << v.size() << endl;
-
   v.push_back(a);
-  cout << "Cantidad: " << v.size() << endl;
 
+  vector<TPoro> x(v);
+  vector<TPoro> z;
+
+  cout << "******TESTING CONSTRUCTORS, OPERATOR[], OPERATOR= AND GETTERS******" << endl;
+
+  cout << p.capacity() << ", "<< p.size() << endl;
+  cout << p << endl;
+
+  cout << b.capacity() << ", "<< b.size() << endl;
+  cout << b << endl;
+
+  cout << v.capacity() << ", "<< v.size() << endl;
   v.push_back(a);
-  cout << "Cantidad: " << v.size() << endl;
+  v.push_back(a);
+  cout << v.capacity() << ", "<< v.size() << endl;
+  cout << v << endl;
+
+  cout << x.capacity() << ", "<< x.size() << endl;
+  cout << x << endl;
+
+  z = v;
+  cout << z.capacity() << ", "<< z.size() << endl;
+  cout << z << endl;
 }
